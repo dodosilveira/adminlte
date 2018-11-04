2a5005ea709d9b1d1783081045a60fad96b477cc
wollyee <douglas.infa@gmail.com>
2018-11-04 03:48:54 -0200

    Laravel com AdminLTE

---
+++ resources/views/home.blade.php
@@ -0,0 +1,23 @@
+@extends('adminlte::page')
+
+@section('title', 'Dashboard')
+
+@section('content_header')
+	<div class="container-fluid">
+	    <h1>Dashboard</h1>
+	</div>
+@stop
+
+@section('content')
+	<div class="container-fluid">
+	    <p>Sistema de contratos.</p>
+	</div>
+@stop
+
+@section('css')
+    {{-- <link rel="stylesheet" href="/css/admin_custom.css"> --}}
+@stop
+
+@section('js')
+    <script> </script>
+@stop
